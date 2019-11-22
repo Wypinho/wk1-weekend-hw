3d527789bb412b82d197b0a70e6a2f52c2a1ff74
Andrew Wyper <andy_wyper@yahoo.co.uk>
2019-11-22 16:56:47 +0000

    optional 4 done

--- pet_shop.rb
+++ pet_shop.rb
@@ -76,3 +76,10 @@ def customer_can_afford_pet(customer, new_pet)
     return false
   end
 end
+
+def sell_pet_to_customer(shop, pet, customer)
+  add_pet_to_customer(customer, pet)
+  increase_pets_sold(shop, 1)
+  remove_customer_cash(customer, pet[:price])
+  add_or_remove_cash(shop, pet[:price])
+end
