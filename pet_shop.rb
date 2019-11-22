db3759a5f36db824635c822a8b04bec7d793ca16
Andrew Wyper <andy_wyper@yahoo.co.uk>
2019-11-22 15:43:08 +0000

    first 17 done

--- pet_shop.rb
+++ pet_shop.rb
@@ -60,3 +60,11 @@ end
 def remove_customer_cash(customer, amount)
   return customer[:cash] -= amount
 end
+
+def customer_pet_count(customer)
+  return customer[:pets].length
+end
+
+def add_pet_to_customer(customer, new_pet)
+  return customer[:pets] << new_pet
+end
