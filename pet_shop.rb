01f5da7d3820ea7a1bc0a0371d3266a8d8b9b226
Andrew Wyper <andy_wyper@yahoo.co.uk>
2019-11-22 12:40:32 +0000

    first 6 done

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,6 +6,14 @@ def total_cash(shop)
   return shop[:admin][:total_cash]
 end
 
-def add_or_remove_cash(shop,amount)
+def add_or_remove_cash(shop, amount)
   return shop[:admin][:total_cash] += amount
+end
+
+def pets_sold(shop)
+  return shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(shop, amount)
+  return shop[:admin][:pets_sold] += amount
 end
